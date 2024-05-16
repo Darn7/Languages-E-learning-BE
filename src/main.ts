fc2a15f20ea10dc4e80439efd761fe8578168b2e
UIT-20521229 <20521229@gm.uit.edu.vn>
2024-05-17 00:27:50 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { SwaggerConfig } from './config';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     //add middleware here!!!!
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+        }),
+    );
     SwaggerConfig.init(app);
     await app.listen(3007);
 }
